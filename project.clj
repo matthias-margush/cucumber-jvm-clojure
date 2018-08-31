d6de31b19d67d349e8fa394c99898cdf2b0cb433
matthias-margush <matthias.margush@fundingcircle.com>
2018-08-30 17:49:13 -0700

    aot compile clj.clj

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject org.clojars.matthiasmargush/cucumber-clojure "2.0.3-SNAPSHOT"
+(defproject org.clojars.matthiasmargush/cucumber-clojure "2.0.5-SNAPSHOT"
   :description "Cucumber implementation for clojure"
   :url "https://github.com/cucumber/cucumber-jvm-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-junit "1.1.8"]]
+  :aot [cucumber.runtime.clj]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [io.cucumber/cucumber-core "2.0.1"]
                  [io.cucumber/cucumber-junit "2.0.1"]
-                 [io.cucumber/cucumber-clojure "2.0.1"]
                  [junit/junit "4.12"]]
   :source-paths ["clojure/src/main/clojure"]
   :java-source-paths ["clojure/src/main/java"
